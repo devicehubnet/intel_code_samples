14c3d00f191e25e9659abba3153b24b711595c3c
Gabriel Arnautu <gabriel@devicehub.net>
2017-05-13 05:43:52 +0000

    finish 4

--- mqtt/python/4_get_digital_actuator.py
+++ mqtt/python/4_get_digital_actuator.py
@@ -16,6 +16,7 @@ by Mihnea Moldovan
 from devicehub.devicehub import Sensor, Actuator, Device, Project
 from random import randint
 from time import sleep
+import mraa
 
 
 PROJECT_ID = '13156'
@@ -25,9 +26,11 @@ API_KEY = '209b0ec1-d2ef-4d01-b915-81688b540bfc'
 
 BUZZER_DIGITAL_ACTUATOR_NAME = "Buzzer_power"
 
+buzzer_power_port = mraa.Gpio(3)
+buzzer_power_port.dir(mraa.DIR_OUT)
 
 def on_actuator_change(data):
-    buzzer_power_port.write(BUZZER_ACTUATOR.state)
+    buzzer_power_port.write(int(BUZZER_ACTUATOR.state))
 
 
 project = Project(PROJECT_ID, persistent = False)
@@ -37,8 +40,6 @@ device = Device(project, DEVICE_UUID, API_KEY)
 BUZZER_ACTUATOR = Actuator(Actuator.DIGITAL, BUZZER_DIGITAL_ACTUATOR_NAME)
 device.addActuator(BUZZER_ACTUATOR, on_actuator_change)
 
-buzzer_power_port = mraa.Gpio(3)
-buzzer_power_port.dir(mraa.DIR_OUT)
 
 while True:
     sleep(1)
